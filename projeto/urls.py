06dd808b4899989fe64745e81cb405875c1c7d21
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2023-12-29 16:32:16 +0100

    created app recipes, and updated the urls

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,24 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import include, path
 
 
-def home(request):
-    ...
-    return HttpResponse('HOME')
-
-def contato(request):
-    ...
-    return HttpResponse('CONTATO')
-
-def sobre(request):
-    ...
-    return HttpResponse('SOBRE')
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+    path('admin', admin.site.urls),
+    path('', include('recipes.urls')),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
 # Create your views here.
+def home(request):
+    ...
+    return HttpResponse('HOME 2')
+
+def contato(request):
+    ...
+    return HttpResponse('CONTATO')
+
+def sobre(request):
+    ...
+    return HttpResponse('SOBRE')
