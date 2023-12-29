d4f3dcfbff29c3477289d13b28fc21cef4ba439c
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2023-12-29 20:20:03 +0100

    create a folder called partials, and use include to home page

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Deyvison',
     })
