6b31bee700d6b6825f8d40702f1a945464cc8675
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2024-01-02 22:40:35 +0100

    new page of cards

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Deyvison',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Deyvison',
+    })
