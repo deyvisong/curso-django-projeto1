086dc86379a741c171ab53fdf3e0eafd8d9ac72c
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2024-01-03 23:29:24 +0100

    replaced code

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for projeto project.
+"""projeto URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
-# Create your views here.
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Deyvison',
+        'name': 'Luiz Otávio',
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Deyvison',
+        'name': 'Luiz Otávio',
     })
